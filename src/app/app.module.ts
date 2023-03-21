8b42d82126a62d3df18e4a659b74cb092d1c909d
dani-cuda <daniel.cuda.17@gmail.com>
2023-03-21 20:37:55 -0300

    Finished class 01 proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FormComponent } from './form/form.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    FormComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
